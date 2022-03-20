548612d59bb4ff27184450cebcfd3754978d9b5e
shadowygaming <mario.64express@gmail.com>
2022-03-20 15:14:48 -0500

    fixing 1.18.2 hopefully

--- src/main/java/com/shadowygamer/bladesedge/util/ModTags.java
+++ src/main/java/com/shadowygamer/bladesedge/util/ModTags.java
@@ -1,15 +1,12 @@
 package com.shadowygamer.bladesedge.util;
 
-
 import com.shadowygamer.bladesedge.BladesEdge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.Tags;
 
 public class ModTags {
         public static class Blocks {
