40d592436e86951b585ba878175f9ddbe14165df
Shadowygamer <mario.64express@gmail.com>
2024-06-27 13:56:03 -0500

    updated forge version to 40.2.21

--- src/main/java/com/shadowygamer/bladesedge/block/ModFlammableRotatedPillarBlock.java
+++ src/main/java/com/shadowygamer/bladesedge/block/ModFlammableRotatedPillarBlock.java
@@ -3,7 +3,6 @@ package com.shadowygamer.bladesedge.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
